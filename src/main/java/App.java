37327d1899c335e3f1a27ed7c0a8973c8cc2518b
Dmitrii <piligrimm.seo@gmail.com>
2023-07-06 11:09:29 +0300

    App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,12 +12,12 @@ public class App {
         HelloWorld helloWorldBeanTwo =
                 (HelloWorld) applicationContext.getBean("helloworld");
 
-        System.out.println("Переменные helloWorldbean и helloWorldbeanTwo ссылаются на один объект? " +
-                (helloWorldBbean == helloWorldBeanTwo));
-
         Cat catOne = (Cat) applicationContext.getBean("cat");
 
         Cat catTwo = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Переменные helloWorldbean и helloWorldbeanTwo ссылаются на один объект? " +
+                (helloWorldBbean == helloWorldBeanTwo));
 
         System.out.println("Переменные catOne и catTwo ссылаются на один объект? " +
                 (catOne == catTwo));
